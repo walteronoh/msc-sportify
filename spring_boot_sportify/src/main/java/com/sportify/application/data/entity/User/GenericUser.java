82015a81ddfa00f54fd3553db1d2fa98f928cf86
Paul Mbuthia <paulmbuthia754@gmail.com>
2023-11-22 18:14:28 +0300

    feat: Added Annotations to booking class

--- spring_boot_sportify/src/main/java/com/sportify/application/data/entity/User/GenericUser.java
+++ spring_boot_sportify/src/main/java/com/sportify/application/data/entity/User/GenericUser.java
@@ -1,5 +1,7 @@
 package com.sportify.application.data.entity.User;
 
+import com.sportify.application.data.entity.AbstractEntity;
+
 public abstract class GenericUser extends AbstractEntity {
     
 }
