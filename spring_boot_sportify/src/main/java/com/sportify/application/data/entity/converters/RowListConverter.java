7e9772e55ea6a826fec9c26666d3ed7c0375fc5f
Paul Mbuthia <paulmbuthia754@gmail.com>
2023-11-22 18:47:48 +0300

    feat: Added RowListConverter

---
+++ spring_boot_sportify/src/main/java/com/sportify/application/data/entity/converters/RowListConverter.java
@@ -0,0 +1,26 @@
+package com.sportify.application.data.entity.converters;
+
+import java.util.Arrays;
+import java.util.List;
+
+import jakarta.persistence.AttributeConverter;
+
+public class RowListConverter implements AttributeConverter<List<Integer>, String> {
+    @Override
+    public String convertToDatabaseColumn(List<Integer> rows) {
+        StringBuilder res = new StringBuilder();
+        for(Integer i: rows) {
+            res.append(i);
+            res.append(" ");
+        }
+        return res.toString();
+    }
+
+    @Override
+    public List<Integer> convertToEntityAttribute(String dbData) {
+        String[] tokens = dbData.split(" ");
+        return Arrays.stream(tokens)
+                .map(Integer::parseInt)
+                .toList();
+    }
+}
