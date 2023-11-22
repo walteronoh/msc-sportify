159273a4994ae1b9dffeb157bc18c0f81f375810
Paul Mbuthia <paulmbuthia754@gmail.com>
2023-11-22 18:43:45 +0300

    feat: Added Reservation definitions

---
+++ spring_boot_sportify/src/main/java/com/sportify/application/data/entity/SeatingSection.java
@@ -0,0 +1,12 @@
+package com.sportify.application.data.entity;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class SeatingSection extends AbstractEntity {
+
+    public int getPrice() {
+        return 0;
+    }
+    
+}
