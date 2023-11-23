c7863d95554a2a52b642dbb647b1d0a99557710e
Paul Mbuthia <paulmbuthia754@gmail.com>
2023-11-23 12:16:31 +0300

    feat: Add Series class details

--- spring_boot_sportify/src/main/java/com/sportify/application/data/entity/event/Series.java
+++ spring_boot_sportify/src/main/java/com/sportify/application/data/entity/event/Series.java
@@ -3,6 +3,7 @@ package com.sportify.application.data.entity.event;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
 import com.sportify.application.data.entity.AbstractEntity;
@@ -42,12 +43,7 @@ public class Series extends AbstractEntity implements Notifiable {
     public void setDescription(String description) {
         this.description = description;
     }
-    public List<Game> getGames() {
-        return new ArrayList<>(games);
-    }
-    public void setGames(List<Game> games) {
-        this.games = games;
-    }
+
     @Override
     public void makeNotification(String description, Severity severity) {
         this.addNotice(new Notice(this, description, severity));
@@ -61,5 +57,52 @@ public class Series extends AbstractEntity implements Notifiable {
         this.notices.add(notice);
     }
 
+    public List<Game> getGames() {
+        return new ArrayList<>(games);
+    }
+    public void setGames(List<Game> games) {
+        this.games = games;
+    }
+    public void addGame(Game g) {
+        this.games.add(g);
+    }
+
+    protected String seriesIdentifier(Integer n) {
+        return "Game Week ".concat(n.toString());
+    }
+    public Optional<String> getGameIdentifier(Game g) {
+        int index = this.games.indexOf(g);
+        if (index == -1) {
+            return Optional.empty();
+        }
+        else {
+            return Optional.of( this.seriesIdentifier(index + 1));
+        }
+    }
+
+    public Optional<Game> getNextGame (Game g) {
+        var index = this.games.indexOf(g);
+        if (index == -1) { //Game g not in series
+            return Optional.empty();
+        }
+        else if (this.games.size() == (index + 1)) { //Game g is last game
+            return Optional.empty();
+        }
+        else {
+            return Optional.of(this.games.get(index + 1));
+        }
+    }
+    public Optional<Game> getPrevGame (Game g) {
+        var index = this.games.indexOf(g);
+        if (index == -1) { //Game g not in series
+            return Optional.empty();
+        }
+        else if (index == 0) { //Game g is first game
+            return Optional.empty();
+        }
+        else {
+            return Optional.of(this.games.get(index - 1));
+        }
+    }
     
 }
